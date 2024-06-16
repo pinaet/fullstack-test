1405712110c4cb51b6fc06ecf54292e7736a0991
Pinaet <pinaet@gmail.com>
2024-06-16 14:22:44 +0700

    set 25 perpage as default

--- html/app/Http/Controllers/Api/PropertyController.php
+++ html/app/Http/Controllers/Api/PropertyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Property;
+use Illuminate\Http\Request;
 use App\Http\Filters\PropertyFilter;
 use App\Http\Resources\PropertyResource;
 use App\Http\Controllers\Controller;
@@ -12,20 +13,25 @@ class PropertyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(PropertyFilter $filters)
+    public function index(Request $request, PropertyFilter $filters)
     {
-        return PropertyResource::collection(Property::filter($filters)->paginate());
+        $perPage = $request->input('per_page', 25); // Default to 25 if not provided
+        return PropertyResource::collection(Property::filter($filters)->paginate($perPage));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show($province, PropertyFilter $filters)
+    public function show(Request $request, $province, PropertyFilter $filters)
     {
         $properties = Property::whereHas('geo', function ($query) use ($province) {
             $query->where('province', $province);
         });
-        return PropertyResource::collection($properties->filter($filters)->paginate());
+
+        // dd($properties->get(), count($properties->get()));
+
+        $perPage = $request->input('per_page', 25); // Default to 25 if not provided
+        return PropertyResource::collection($properties->filter($filters)->paginate($perPage));
 
     }
 }
